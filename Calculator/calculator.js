7b85eb75d6a5393f77de232fd9aa35b4750b9905
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2022-10-08 11:43:51 +0200

    calculator completed

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,9 +1,16 @@
-const numbers = document.querySelectorAll(".nb");
+const buttons = document.querySelectorAll(".btn");
 const result = document.querySelector("h3");
 
-numbers.forEach((nb) => {
-  nb.addEventListener("click", (e) => {
-    result.textContent = e.target.textContent;
-    console.log(e.target.textContent);
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    result.textContent += e.target.textContent;
   });
+});
+
+equal.addEventListener("click", () => {
+  result.textContent = eval(result.textContent);
+});
+
+clear.addEventListener("click", () => {
+  result.textContent = "";
 });
