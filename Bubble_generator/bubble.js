9829ad5cb758589f95901ad25a18adb79ee57864
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2022-10-24 14:44:37 +0200

    feat : animation + setInterval

--- Bubble_generator/bubble.js
+++ Bubble_generator/bubble.js
@@ -1,22 +1,26 @@
-const bubble = document.createElement("span");
-const randomSize = Math.random() * 200 + 100 + "px";
-const randomTop = Math.random() * 100 + "%";
-const randomLeft = Math.random() * 100 + "%";
-const randomColor =
-  "rgb(" +
-  Math.random() * 255 +
-  "," +
-  Math.random() * 255 +
-  "," +
-  Math.random() * 255 +
-  ")";
+const generator = () => {
+  const bubble = document.createElement("span");
+  const randomSize = Math.random() * 200 + 100 + "px";
+  const randomTop = Math.random() * 100 + 50 + "%";
+  const randomLeft = Math.random() * 100 + "%";
+  const randomColor =
+    "rgb(" +
+    Math.random() * 255 +
+    "," +
+    Math.random() * 255 +
+    "," +
+    Math.random() * 255 +
+    ")";
 
-bubble.classList.add("bubble");
-document.body.appendChild(bubble);
+  bubble.classList.add("bubble");
+  document.body.appendChild(bubble);
 
-bubble.style.height = randomSize;
-bubble.style.width = randomSize;
-bubble.style.background = randomColor;
-bubble.style.background = randomColor;
-bubble.style.top = randomTop;
-bubble.style.left = randomLeft;
+  bubble.style.height = randomSize;
+  bubble.style.width = randomSize;
+  bubble.style.background = randomColor;
+  bubble.style.background = randomColor;
+  bubble.style.top = randomTop;
+  bubble.style.left = randomLeft;
+};
+
+setInterval(generator, 300);
