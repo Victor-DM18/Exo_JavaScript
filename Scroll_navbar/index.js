ef96d8c519870778ff3ac8a63bdc28d5ddbd0315
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2022-10-29 13:08:01 +0200

    feat : add scroll event

--- Scroll_navbar/index.js
+++ Scroll_navbar/index.js
@@ -5,3 +5,15 @@
 // Stocker la valeur du précédent niveau de scroll pour savoir si l'on est monté ou descendu
 
 // Connaitre niveau de scroll (window.scrollY)
+
+const navbar = document.querySelector("#navbar");
+let scroll = 0;
+
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY > scroll) {
+    navbar.style.top = "-60px";
+  } else {
+    navbar.style.top = "0px";
+  }
+  scroll = window.scrollY;
+});
