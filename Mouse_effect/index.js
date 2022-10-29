046c1fda160c843fbd72dcb429d385e6c22afb90
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2022-10-29 10:32:27 +0200

    code optimase with forEach

--- Mouse_effect/index.js
+++ Mouse_effect/index.js
@@ -1,20 +1,23 @@
-// Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
+// const mousemoveBlack = document.querySelector(".mousemoveBlack");
+// const mousemoveWhite = document.querySelector(".mousemoveWhite");
+// const mouse = document.querySelector(".mouse");
 
-// Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
+// window.addEventListener("mousemove", (e) => {
+//   mousemoveBlack.style.top = e.pageY + "px";
+//   mousemoveBlack.style.left = e.pageX + "px";
+//   mousemoveWhite.style.top = e.pageY + "px";
+//   mousemoveWhite.style.left = e.pageX + "px";
+//   mouse.style.top = e.pageY + "px";
+//   mouse.style.left = e.pageX + "px";
+// });
 
-// S'assurer que les liens sont clickables
+// code optimisé
 
-// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
-
-const mousemoveBlack = document.querySelector(".mousemoveBlack");
-const mousemoveWhite = document.querySelector(".mousemoveWhite");
-const mouse = document.querySelector(".mouse");
+const mouses = document.querySelectorAll(".mousemove");
 
 window.addEventListener("mousemove", (e) => {
-  mousemoveBlack.style.top = e.pageY + "px";
-  mousemoveBlack.style.left = e.pageX + "px";
-  mousemoveWhite.style.top = e.pageY + "px";
-  mousemoveWhite.style.left = e.pageX + "px";
-  mouse.style.top = e.pageY + "px";
-  mouse.style.left = e.pageX + "px";
+  mouses.forEach((mouse) => {
+    mouse.style.left = e.pageX + "px";
+    mouse.style.top = e.pageY + "px";
+  });
 });
