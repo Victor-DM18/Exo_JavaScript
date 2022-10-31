4a76bf51d1c7dd9d7765ecfdc994dca0a92acf14
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2022-10-31 12:44:45 +0100

    feat : event scroll & click

--- scroll_popUp/script.js
+++ scroll_popUp/script.js
@@ -7,3 +7,31 @@
 // Faire apparaitre la popup quand on est en bas du site
 
 // Bonus : quand on clicke sur la popup elle disparait pour toujours
+
+const navbar = document.getElementById("navbar");
+const image = document.getElementById("imgImprovise");
+const popup = document.getElementById("popup");
+
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  if (window.scrollY > 0) {
+    navbar.style.height = "60px";
+  } else {
+    navbar.style.height = "90px";
+  }
+
+  if (window.scrollY > 100) {
+    image.style.opacity = "1";
+  } else {
+    image.style.opacity = "0";
+  }
+
+  if (window.scrollY >= 1200) {
+    popup.style.opacity = "1";
+    popup.style.transform = "translate(0px)";
+  }
+});
+
+popup.addEventListener("click", () => {
+  popup.remove();
+});
