73deed3908f67cbd81c7781c75e8d06a6bb09ff6
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2022-10-31 11:57:21 +0100

    initial commit scroll popup

--- Scroll_navbar/index.js
+++ Scroll_navbar/index.js
@@ -1,11 +1,3 @@
-// Créer un événément au scroll
-
-// Cacher la navbar si l'utilisateur comment à descendre et la sortir quand il remonte
-
-// Stocker la valeur du précédent niveau de scroll pour savoir si l'on est monté ou descendu
-
-// Connaitre niveau de scroll (window.scrollY)
-
 const navbar = document.querySelector("#navbar");
 let scroll = 0;
 
