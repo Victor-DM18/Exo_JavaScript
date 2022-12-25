530f576572da0b1eb4023054cd87c53e6cf2ceb8
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2022-12-25 19:15:43 +0100

    initial commit generator password

--- La_Data/Animation_text/index.js
+++ La_Data/Animation_text/index.js
@@ -10,29 +10,36 @@ let array = [
 let wordIndex = 0;
 let letterIndex = 0;
 
-const createWord = () => {
+const createLetter = () => {
   const letter = document.createElement("span");
   target.appendChild(letter);
 
-  letter.textContent = array[wordIndex];
+  letter.textContent = array[wordIndex][letterIndex];
   setTimeout(() => {
+    target.style.opacity = 0;
     letter.remove();
   }, 3000);
 };
 
 const display = () => {
-  if (wordIndex >= array.length) {
-    wordIndex = 0;
-    letterIndex = 0;
-    display();
-  } else {
-    createWord();
-    wordIndex++;
-    letterIndex = 0;
-    setTimeout(() => {
+  setTimeout(() => {
+    if (wordIndex >= array.length) {
+      wordIndex = 0;
+      letterIndex = 0;
       display();
-    }, 3000);
-  }
+    } else if (letterIndex < array[wordIndex].length) {
+      target.style.opacity = 1;
+      createLetter();
+      letterIndex++;
+      display();
+    } else {
+      wordIndex++;
+      letterIndex = 0;
+      setTimeout(() => {
+        display();
+      }, 3000);
+    }
+  }, 100);
 };
 
 display();
