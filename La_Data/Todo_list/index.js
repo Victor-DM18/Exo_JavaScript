6f18d9d4f05d2a830a2b1f41a316e58e14bfd0f4
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-01-21 18:37:24 +0100

    feat : localStorage + getList

--- La_Data/Todo_list/index.js
+++ La_Data/Todo_list/index.js
@@ -1,12 +1,26 @@
 const form = document.querySelector(".form");
 const list = document.getElementById("list");
 
+const storage = () => {
+  window.localStorage.todoList = list.innerHTML;
+};
+
+const getList = () => {
+  list.innerHTML = window.localStorage.todoList;
+};
+
+getList();
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   list.innerHTML += `<li>${todo.value}</li>`;
   todo.value = "";
+  storage();
+});
 
-  list.addEventListener("click", (e) => {
-    e.target.remove();
-  });
+list.addEventListener("click", (e) => {
+  document.querySelector("span")
+    ? e.target.remove()
+    : (e.target.innerHTML += "<span> OK </span>");
+  storage();
 });
