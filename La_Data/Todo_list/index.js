3c2f1aad43318ebcab6525e842ce4cec625f3895
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-01-21 17:19:10 +0100

    feat : remove todo when click + form update

--- La_Data/Todo_list/index.js
+++ La_Data/Todo_list/index.js
@@ -1,21 +1,12 @@
 const form = document.querySelector(".form");
-const input = document.getElementById("todo");
-const todos = document.querySelectorAll("span");
-input.addEventListener("keypress", (e) => {
-  e.preventDefault();
-  console.log(e.key);
-  if (e.key === "Enter") {
-    const spans = form.appendChild(document.createElement("span"));
-    spans.textContent = `- ${input.value}`;
-    input.value = "";
-  } else {
-    input.value += e.key;
-  }
-});
+const list = document.getElementById("list");
 
-todos.forEach((span) => {
-  form.addEventListener("click", (e) => {
-    e.target = span ? span.remove() : null;
-    console.log(e.target);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  list.innerHTML += `<li>${todo.value}</li>`;
+  todo.value = "";
+
+  list.addEventListener("click", (e) => {
+    e.target.remove();
   });
 });
