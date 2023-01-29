3d6b49a6fdcfa1622b1cd15a13d8686836f3e48e
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-01-29 13:06:14 +0100

    feat : method GET + display data

--- Les_API/test_server_json/index.js
+++ Les_API/test_server_json/index.js
@@ -1,3 +1,13 @@
+fetch("http://localhost:3000/posts").then((res) => {
+  res.json().then((data) => {
+    for (i = 0; i < data.length; i++) {
+      post.innerHTML += `<div id=box> <span id=pseudo>pseudo : ${data[i].pseudo}</span>     
+      <span id=age> age : ${data[i].age}</span>
+      <span id=message>message : ${data[i].message}</span></div>`;
+    }
+  });
+});
+
 const init = {
   method: "POTS",
   headers: {
@@ -13,7 +23,7 @@ const init = {
 };
 
 document.querySelector("form").addEventListener("submit", () => {
-  fetch("http://localhost:300/posts", init).then((data) => {
+  fetch("http://localhost:3000/posts", init).then((data) => {
     console.log("data envoyé");
   });
 });
