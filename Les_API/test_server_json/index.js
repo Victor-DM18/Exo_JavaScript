673d705d29edcf644760668242a94a481f656e0a
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-01-30 23:27:39 +0100

    feat : add post by form

--- Les_API/test_server_json/index.js
+++ Les_API/test_server_json/index.js
@@ -14,23 +14,50 @@ fetch("http://localhost:3000/posts").then((res) => {
   });
 });
 
-const init2 = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    pseudo: pseudoValue,
-    message: messageValue,
-    age: ageValue,
-  }),
-  mode: "cors",
-  credentials: "same-origin",
+const pseudoCheck = (value) => {
+  pseudoValue = value;
+};
+
+const ageCheck = (value) => {
+  ageValue = value;
+};
+
+const messageCheck = (value) => {
+  messageValue = value;
 };
 
 //event
 
+inputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    switch (e.target.id) {
+      case "pseudoText":
+        pseudoCheck(e.target.value);
+        break;
+      case "message":
+        messageCheck(e.target.value);
+        break;
+      case "age":
+        ageCheck(e.target.value);
+        break;
+    }
+  });
+});
+
 form.addEventListener("submit", (e) => {
+  const init2 = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      pseudo: pseudoValue,
+      message: messageValue,
+      age: ageValue,
+    }),
+    mode: "cors",
+    credentials: "same-origin",
+  };
   if (pseudoValue !== "" && messageValue !== "" && ageValue !== "") {
     fetch("http://localhost:3000/posts", init2).then((e) => {
       e.preventDefault();
@@ -39,20 +66,4 @@ form.addEventListener("submit", (e) => {
   } else {
     alert("erreur formulaire !!");
   }
-});
-
-inputs.forEach((input) => {
-  input.addEventListener("input", (e) => {
-    switch (e.target.id) {
-      case "pseudoText":
-        pseudoValue = e.target.value;
-        break;
-      case "message":
-        messageValue = e.target.value;
-        break;
-      case "age":
-        ageValue = e.target.value;
-        break;
-    }
-  });
 });
