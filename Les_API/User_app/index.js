a81a6785fda37cd2b6c6c53a67cf21a48af4e0f5
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-02-02 20:28:12 +0100

    update style

--- Les_API/User_app/index.js
+++ Les_API/User_app/index.js
@@ -4,10 +4,18 @@ const fetchUser = async () => {
   await fetch("https://randomuser.me/api/?results=24").then((res) =>
     res.json().then((data) => {
       users = data.results;
-      console.log();
     })
   );
 };
+
+function dateParser(chaine) {
+  let newDate = new Date(chaine).toLocaleDateString("fr-FR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  return newDate;
+}
 
 const displayUsers = async () => {
   await fetchUser();
@@ -15,15 +23,18 @@ const displayUsers = async () => {
   usersList.innerHTML = users
     .map(
       (user) => `
-      <div id="list"> 
-      <img src="${user.picture.large}"/>
-      <h3>${user.name.first} ${user.name.last.toUpperCase()}</h3>
-      <span>Sexe : ${user.gender}</span>
-      <span>Âge : ${user.dob.age}</span>
-      <span>Email : ${user.email}</span>
-      <span>Tél : ${user.phone}</span>
-      <span>Inscris depuis : ${user.registered.date.split("T")[0]}</span>
-      </div>
+    <div id="list"> 
+    <img src="${user.picture.large}"/>
+    <h3>${user.name.first} ${user.name.last.toUpperCase()}</h3>
+    <span>Sexe : ${user.gender}</span>
+    <span>Âge : ${user.dob.age}</span>
+    <span>Email : ${user.email}</span>
+    <span>Tél : ${user.phone}</span>
+    <span>Inscris depuis : ${user.registered.date
+      .split("T")[0]
+      .split("-")
+      .join("/")}</span>
+    </div>
     
     `
     )
