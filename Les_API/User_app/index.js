842a905700d4b10e0dc19cfa8772a7e3b0ffb6a2
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-02-02 18:28:20 +0100

    update description

--- Les_API/User_app/index.js
+++ Les_API/User_app/index.js
@@ -4,7 +4,7 @@ const fetchUser = async () => {
   await fetch("https://randomuser.me/api/?results=24").then((res) =>
     res.json().then((data) => {
       users = data.results;
-      console.log(users);
+      console.log();
     })
   );
 };
@@ -18,10 +18,11 @@ const displayUsers = async () => {
       <div id="list"> 
       <img src="${user.picture.large}"/>
       <h3>${user.name.first} ${user.name.last.toUpperCase()}</h3>
-      <span>sex : ${user.gender}</span>
-      <span>âge : ${user.dob.age}</span>
-      <span>email : ${user.email}</span>
-      <span>tél : ${user.phone}</span>
+      <span>Sexe : ${user.gender}</span>
+      <span>Âge : ${user.dob.age}</span>
+      <span>Email : ${user.email}</span>
+      <span>Tél : ${user.phone}</span>
+      <span>Inscris depuis : ${user.registered.date.split("T")[0]}</span>
       </div>
     
     `
