3e8fcf30a1b4f9fdb6d603cdd5f3b70ba87c763c
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-02-03 14:27:10 +0100

    update calcul day

--- Les_API/User_app/index.js
+++ Les_API/User_app/index.js
@@ -9,26 +9,26 @@ const fetchUser = async () => {
   );
 };
 
-function dateParser(chaine) {
+const dateParser = (chaine) => {
   let newDate = new Date(chaine).toLocaleDateString("fr-FR", {
     year: "numeric",
     month: "long",
     day: "numeric",
   });
   return newDate;
-}
+};
+
+const calculDay = (day) => {
+  let today = new Date();
+  let todayParse = Date.parse(today);
+  let dayParse = Date.parse(day);
+  let diff = Math.ceil((todayParse - dayParse) / 8.64e7);
+
+  return diff;
+};
 
 const displayUsers = async () => {
   await fetchUser();
-
-  function dateParser(chaine) {
-    let newDate = new Date(chaine).toLocaleDateString("fr-FR", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    });
-    return newDate;
-  }
 
   usersList.innerHTML = users
     .map(
@@ -37,11 +37,11 @@ const displayUsers = async () => {
     <img src="${user.picture.large}"/>
     <h3>${user.name.first} ${user.name.last.toUpperCase()}</h3>
     <span>Sexe : ${user.gender == "male" ? "homme" : "femme"}</span>
-    <span>Âge : ${user.dob.age} ans</span>
+    <span>Né le  : ${dateParser(user.dob.date)}</span>
     <span>Email : ${user.email}</span> 
     <span>Vie à : ${user.location.city}, ${user.location.country}</span> 
     <span>Tél : ${user.phone}</span>
-    <span>Inscris depuis le : ${dateParser(user.registered.date)}</span>
+    <span>Inscris depuis ${calculDay(user.registered.date)} jours</span>
     </div>
     
     `
