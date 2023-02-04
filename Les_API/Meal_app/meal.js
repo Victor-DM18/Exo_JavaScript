dc030ffac4fb1fc8de4cbc872a71136823dcf534
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-02-04 17:15:47 +0100

    feat : display ingredients + style

--- Les_API/Meal_app/meal.js
+++ Les_API/Meal_app/meal.js
@@ -5,19 +5,51 @@ const fetchData = async () => {
     .then((res) => res.json())
     .then((data) => {
       recipies = data.meals;
-      console.log(recipies[0]);
+      console.log(recipies);
     });
 };
 
 const displayMeal = async () => {
   await fetchData();
 
-  meal.innerHTML = recipies.map(
-    (recipe) =>
+  meal.innerHTML = recipies
+    .map(
+      (recipe) =>
+        `
+        <div id="recipe">
+      <h2>${recipe.strMeal}<h2>
+      <span>${recipe.strIngredient1}</span>
+      <span>${recipe.strIngredient2}</span>
+      <span>${recipe.strIngredient3}</span>
+      <span>${recipe.strIngredient4}</span>
+      <span>${recipe.strIngredient5}</span>
+      <span>${recipe.strIngredient6}</span>
+      <span>${recipe.strIngredient7}</span>
+      <span>${recipe.strIngredient8}</span>
+      <span>${recipe.strIngredient9}</span>
+      <span>${recipe.strIngredient10}</span>
+      <span>${recipe.strIngredient11}</span>
+      <span>${recipe.strIngredient12}</span>
+      <span>${recipe.strIngredient13}</span>
+      <span>${recipe.strIngredient14}</span>
+      <span>${recipe.strIngredient15}</span>
+      <span>${recipe.strIngredient16}</span>
+      <span>${recipe.strIngredient17}</span>
+      <span>${recipe.strIngredient18}</span>
+      <span>${recipe.strIngredient19}</span>
+      <span>${recipe.strIngredient20}</span>
+      <span>${recipe.strIngredient21}</span>
+      <span>${recipe.strIngredient22}</span>
+      <span>${recipe.strIngredient23}</span>
+      <span>${recipe.strIngredient24}</span>
+      <span>${recipe.strIngredient25}</span>
+      <span>${recipe.strIngredient26}</span>
+      <span>${recipe.strIngredient27}</span>
+      <span>${recipe.strIngredient28}</span>
+      </div>
       `
-      <spam>${recipe.strArea} ${recipe.strCategory}<span>
-      `
-  );
+    )
+    .join("");
 };
 
 search.addEventListener("submit", (e) => {
