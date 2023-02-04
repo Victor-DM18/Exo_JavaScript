0bed6b1a91a97ccca22f73fe1514a071a0d4ea55
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-02-04 16:17:53 +0100

    feat : display recipies

--- Les_API/Meal_app/meal.js
+++ Les_API/Meal_app/meal.js
@@ -1,12 +1,26 @@
 // https://www.themealdb.com/api/json/v1/1/search.php?s=tomato
+const fetchData = async () => {
+  const value = food.value;
+  await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${value}`)
+    .then((res) => res.json())
+    .then((data) => {
+      recipies = data.meals;
+      console.log(recipies[0]);
+    });
+};
+
+const displayMeal = async () => {
+  await fetchData();
+
+  meal.innerHTML = recipies.map(
+    (recipe) =>
+      `
+      <spam>${recipe.strArea} ${recipe.strCategory}<span>
+      `
+  );
+};
 
 search.addEventListener("submit", (e) => {
   e.preventDefault();
-  const value = food.value;
-  const fetchData = async () => {
-    await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${value}`)
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  };
-  fetchData();
+  displayMeal();
 });
