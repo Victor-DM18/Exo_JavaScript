916587ddfd56cb1dd6734a52c8e477b82cdec402
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-02-06 17:53:05 +0100

    ffature : displayCountries function

--- Les_API/Country_app/country.js
+++ Les_API/Country_app/country.js
@@ -1,9 +1,23 @@
-// 1 - Tester le lien de l'API dans le navigateur (https://restcountries.com/v3.1/all)
+let countries = [];
 
 const dataFetch = async () => {
   await fetch("https://restcountries.com/v3.1/all").then((res) =>
-    res.json().then((data) => console.log(data[0]))
+    res.json().then((data) => {
+      countries = data;
+      console.log(countries[0].name.common);
+    })
   );
 };
 
-dataFetch();
+const displayCountries = async () => {
+  await dataFetch();
+
+  display.innerHTML = countries.map(
+    (country) =>
+      `<div id="box">
+    <h2>${country.name.common}</h2>
+    </div>`
+  );
+};
+
+displayCountries();
