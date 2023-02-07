dd8f4e31b6161adf26ec5986c8ad33098f52ef0d
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-02-07 12:29:41 +0100

    feat : event input + filter name country

--- Les_API/Country_app/country.js
+++ Les_API/Country_app/country.js
@@ -1,4 +1,5 @@
 let countries = [];
+let value = "";
 
 const dataFetch = async () => {
   await fetch("https://restcountries.com/v3.1/all").then((res) =>
@@ -11,25 +12,29 @@ const dataFetch = async () => {
 
 const displayCountries = async () => {
   await dataFetch();
-
   display.innerHTML = countries
+    .filter((e) => e.translations.fra.common.includes(value))
     .map((country) => {
       for (i = 0; i < countries.lrngth; i++) {
         let number = countries[i].population;
         let nb = new Intl.NumberFormat().format(number);
       }
       return `
-        <div id="box">
-        <img src="${country.flags.png}" />
-        <h2>${country.translations.fra.common}</h2>
-        <span>Capitale : <strong>${country.capital}</strong></span>
-        <span>Population : <strong>${new Intl.NumberFormat([
-          "ban",
-          "id",
-        ]).format(country.population)}</strong></span>
-        </div>`;
+  <div id="box">
+  <img src="${country.flags.png}" />
+  <h2>${country.translations.fra.common}</h2>
+  <span>Capitale : <strong>${country.capital}</strong></span>
+  <span>Population : <strong>${new Intl.NumberFormat(["ban", "id"]).format(
+    country.population
+  )}</strong></span>
+  </div>`;
     })
     .join("");
 };
 
 displayCountries();
+
+search.addEventListener("input", (e) => {
+  value = e.target.value;
+  displayCountries();
+});
