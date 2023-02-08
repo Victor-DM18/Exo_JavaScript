449e2d44ea3ab64158e90761ca893043a43e6a75
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-02-08 19:49:16 +0100

    update cours

--- POO/cours/index.js
+++ POO/cours/index.js
@@ -12,20 +12,32 @@ console.log(obj);
 obj.job = "dev web";
 
 console.log("-----------------------------");
-console.log("after added");
+console.log("after added :");
 console.log(obj);
 
 // Modify
-
 obj.pseudo = "Pegi";
 
 console.log("-----------------------------");
-console.log("after modified");
+console.log("after modified :");
 console.log(obj);
 
 // Delete
 delete obj.hobby;
 
 console.log("-----------------------------");
-console.log("after deleted");
+console.log("after deleted :");
 console.log(obj);
+
+//Checked
+
+console.log("-----------------------------");
+console.log("for checking : ");
+console.log("pseudo" in obj, "language" in obj);
+
+// Browse an object
+console.log("-----------------------------");
+console.log("browse an object : ");
+for (const key in obj) {
+  console.log(key + " : " + obj[key]);
+}
