3eab72d6f910f28a05d6808f8a7986787ce6089c
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-02-13 21:22:16 +0100

    docs : add exerciceArray

---
+++ POO/cours/yoga_app/yoga.js
@@ -0,0 +1,42 @@
+const exerciceArray = [
+  {
+    exo: 1,
+    time: 1,
+  },
+  {
+    exo: 2,
+    time: 1,
+  },
+  {
+    exo: 3,
+    time: 1,
+  },
+  {
+    exo: 4,
+    time: 1,
+  },
+  {
+    exo: 5,
+    time: 1,
+  },
+  {
+    exo: 6,
+    time: 1,
+  },
+  {
+    exo: 7,
+    time: 1,
+  },
+  {
+    exo: 8,
+    time: 1,
+  },
+  {
+    exo: 9,
+    time: 1,
+  },
+  {
+    exo: 10,
+    time: 1,
+  },
+];
