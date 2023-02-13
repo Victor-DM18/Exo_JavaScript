136f81e5a0339b9320ef1d2fb9c77856436f7b2f
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-02-13 22:33:42 +0100

    chore : class display function exercices

--- POO/cours/yoga_app/yoga.js
+++ POO/cours/yoga_app/yoga.js
@@ -1,42 +1,22 @@
+const { Title } = require("chart.js");
+
 const exerciceArray = [
-  {
-    exo: 1,
-    time: 1,
-  },
-  {
-    exo: 2,
-    time: 1,
-  },
-  {
-    exo: 3,
-    time: 1,
-  },
-  {
-    exo: 4,
-    time: 1,
-  },
-  {
-    exo: 5,
-    time: 1,
-  },
-  {
-    exo: 6,
-    time: 1,
-  },
-  {
-    exo: 7,
-    time: 1,
-  },
-  {
-    exo: 8,
-    time: 1,
-  },
-  {
-    exo: 9,
-    time: 1,
-  },
-  {
-    exo: 10,
-    time: 1,
-  },
+  { exo: 0, time: 1 },
+  { exo: 1, time: 1 },
+  { exo: 2, time: 1 },
+  { exo: 3, time: 1 },
+  { exo: 4, time: 1 },
+  { exo: 5, time: 1 },
+  { exo: 6, time: 1 },
+  { exo: 7, time: 1 },
+  { exo: 8, time: 1 },
+  { exo: 9, time: 1 },
 ];
+
+const display = {
+  exrcices: function () {
+    title.innerHTML = "Paranétrage";
+    exercice.innerHTML = "";
+    btn.innerHTML = "<button id='start'>START</button>";
+  },
+};
