7cfd0c2ebfec84f812446763ea359f06a1d68d4b
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-02-15 17:42:02 +0100

    feat: flex exercice card

--- POO/yoga_app/yoga.js
+++ POO/yoga_app/yoga.js
@@ -26,7 +26,7 @@ const display = {
         (exercice) =>
           `
       <div id="card">
-      <div>
+      <div id="timer">
       <input type="number" id=${exercice.exo} min="1" max="10" value=${exercice.time}>
       <spam">min</spam>
       </div>
