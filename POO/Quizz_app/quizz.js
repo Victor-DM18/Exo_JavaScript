b1fa39a338b7e933f7825aca351e7d1cd3fdc21c
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-02-21 13:19:52 +0100

    refactor : Questions created

---
+++ POO/Quizz_app/quizz.js
@@ -0,0 +1,20 @@
+const questions = [
+  new Question(
+    "Quelle méthode JavaScript permet de filtrer les élément d'un tableau ?",
+    ["isNaN()", "filter()", "map()", "reduce()"],
+    "filter()"
+  ),
+  new Question(
+    "Quelle méthode transforme du JSON en objet JavaScript ?",
+    ["JSOM.stringify()", "JSON.object()", "JSON.parse()", "JSON.js()"],
+    "JSON.parse()"
+  ),
+  new Question(
+    "Quelle est le resulta du code suivant : << typeof([1, 23, 18]) >> ?",
+    ["Array", "Object", "Number", "undefiined"],
+    "Object"
+  ),
+  new Question("Quelle est le resultat code suivant : << 'Paris' + 18 >> ?"),
+  ["NaN", "undefined", "'Paris18'", "0"],
+  "'Paris18",
+];
