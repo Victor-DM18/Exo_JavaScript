1efee2ed2f61f37676e5b7ea58ee24eb403d7af8
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-02-23 20:42:18 +0100

    feat : logo created

--- Other_things/index.js
+++ Other_things/index.js
@@ -1,30 +1,41 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "rgb(221, 143, 65";
-ctx.fillRect(0, 0, 300, 50);
-
-ctx.fillStyle = "grey";
-ctx.fillRect(0, 0, 50, 300);
-
-ctx.fillStyle = "red";
-ctx.strokeRect(51, 51, 300, 300);
-
 ctx.fillStyle = "black";
 ctx.beginPath();
-ctx.moveTo(52, 52);
-ctx.lineTo(52, 300);
-ctx.lineTo(180, 300);
+ctx.moveTo(0, 0);
+ctx.lineTo(0, 300);
+ctx.lineTo(150, 300);
+ctx.fill();
+
+ctx.beginPath();
+ctx.moveTo(150, 300);
+ctx.lineTo(300, 300);
+ctx.lineTo(300, 0);
+ctx.fill();
+
+ctx.beginPath();
+ctx.moveTo(150, 220);
+ctx.lineTo(40, 0);
+ctx.lineTo(260, 0);
+ctx.fill();
+
+ctx.fillStyle = "grey";
+
+ctx.beginPath();
+ctx.moveTo(0, 60);
+ctx.lineTo(0, 300);
+ctx.lineTo(120, 300);
 ctx.fill();
 
 ctx.beginPath();
 ctx.moveTo(180, 300);
 ctx.lineTo(300, 300);
-ctx.lineTo(300, 52);
+ctx.lineTo(300, 60);
 ctx.fill();
 
 ctx.beginPath();
-ctx.moveTo(180, 220);
-ctx.lineTo(100, 52);
-ctx.lineTo(250, 52);
+ctx.moveTo(150, 180);
+ctx.lineTo(60, 0);
+ctx.lineTo(240, 0);
 ctx.fill();
