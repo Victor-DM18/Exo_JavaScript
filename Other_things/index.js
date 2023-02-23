6c3672a71bb2643d361fd2ec083c9d4ef9544f29
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-02-23 19:16:18 +0100

    feat : test canvas

--- Other_things/index.js
+++ Other_things/index.js
@@ -1,8 +1,30 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "black";
+ctx.fillStyle = "rgb(221, 143, 65";
 ctx.fillRect(0, 0, 300, 50);
 
 ctx.fillStyle = "grey";
 ctx.fillRect(0, 0, 50, 300);
+
+ctx.fillStyle = "red";
+ctx.strokeRect(51, 51, 300, 300);
+
+ctx.fillStyle = "black";
+ctx.beginPath();
+ctx.moveTo(52, 52);
+ctx.lineTo(52, 300);
+ctx.lineTo(180, 300);
+ctx.fill();
+
+ctx.beginPath();
+ctx.moveTo(180, 300);
+ctx.lineTo(300, 300);
+ctx.lineTo(300, 52);
+ctx.fill();
+
+ctx.beginPath();
+ctx.moveTo(180, 220);
+ctx.lineTo(100, 52);
+ctx.lineTo(250, 52);
+ctx.fill();
