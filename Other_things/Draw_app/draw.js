d92ec3fcbb718f5325f693a45607da27bc1f0403
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-02-24 22:20:35 +0100

    feat : event mouse location

--- Other_things/Draw_app/draw.js
+++ Other_things/Draw_app/draw.js
@@ -1,13 +1,16 @@
 const canvas = document.getElementById("draw");
 const ctx = canvas.getContext("2d");
 
-const getLocation = () => {
+const getLocation = (e) => {
   const rect = canvas.getBoundingClientRect();
 
-  console.log(rect);
+  console.log({
+    x: e.clientX - rect.left,
+    y: e.clientY - rect.top,
+  });
 };
 
-canvas.addEventListener("mousdown", (e) => {
+canvas.addEventListener("mousedown", (e) => {
   e.preventDefault();
-  getLocation();
+  getLocation(e);
 });
