c6a595c4cc69933ea6d3b4a4124fc7085eb8efef
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-03-04 17:35:39 +0100

    refactor : all value vreated

---
+++ Bird_Game/bird.js
@@ -0,0 +1,20 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const img = new Image();
+
+img.src = "./media/flappy-bird-set.png";
+
+let displayGame = false;
+
+const gravity = 0.5;
+const speed = 6;
+const size = [51, 36];
+const jump = -11;
+const cTenth = canvas.width / 10;
+
+let index = 0;
+let highScore = 0;
+let currentScore = 0;
+let pipe = [];
+let flight;
+let flyHeight;
