2ac0521b7453284d2cf7d4d5eb697fe54ad25b22
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-03-08 17:10:11 +0100

    docs : variable & array ts

--- _TypeScript/index.js
+++ _TypeScript/index.js
@@ -1,2 +1,26 @@
 "use strict";
 console.log("Hello world !!");
+//-----------
+// variable
+//-----------
+let nb = 18;
+// nb = "viki" => error deteceted
+nb = 8;
+let nb2 = "viki";
+let value;
+value = nb2;
+value = nb;
+// velue = true => error detected
+//-----------
+// Array
+//-----------
+let ar = [18, 8, 29];
+ar = [7];
+// ar = ["viki"]  => error detected
+let arr2 = ["viki", 18];
+arr2 = [18];
+arr2 = ["hello"];
+arr2 = [8, 29, 23, 1];
+let array = [];
+// array = [{ nom: 34 }, false, 18] erroe detected
+array = [4, true, 29];
--- _TypeScript/index.ts
+++ _TypeScript/index.ts
@@ -1,2 +1,46 @@
  console.log("Hello world !!");
  
+ //-----------
+ // variable
+ //-----------
+
+let nb = 18
+
+// nb = "viki" => error deteceted
+
+nb = 8
+
+let nb2 = "viki"
+
+let value: number | string
+
+value = nb2
+
+value = nb
+
+// velue = true => error detected
+
+
+ //-----------
+// Array
+ //-----------
+
+let ar = [18, 8, 29]
+ 
+ar = [7]
+
+// ar = ["viki"]  => error detected
+
+let arr2 = ["viki", 18]
+
+arr2 = [18]
+
+arr2 = ["hello"]
+
+arr2 = [8, 29, 23, 1]
+
+let array: (boolean | number)[] = []
+
+// array = [{ nom: 34 }, false, 18] erroe detected
+
+array = [4, true, 29]
