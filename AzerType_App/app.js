560ef60b9dde8fd84095bebd6422a15e68a83596
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2024-02-17 14:55:54 +0100

    feat : exo test

--- AzerType_App/app.js
+++ AzerType_App/app.js
@@ -1,9 +1,36 @@
-const initialWord = "Paris";
+const initialWord = ["paris", "noir", "renard"];
 
-let sentence = prompt("enter un mot : ");
+let score = 0;
 
-if (sentence === initialWord) {
+let sentence = prompt("enter le premier mot : ");
+
+if (sentence === initialWord[0]) {
+  score++;
   console.log("Well done !");
+  console.log("score = " + score);
 } else {
   console.log("Try again !");
+  console.log("score = " + score);
+}
+
+sentence = prompt("enter le deuxième mot : ");
+
+if (sentence === initialWord[1]) {
+  score++;
+  console.log("Well done !");
+  console.log("score = " + score);
+} else {
+  console.log("Try again !");
+  console.log("score = " + score);
+}
+
+sentence = prompt("enter le troisième mot : ");
+
+if (sentence === initialWord[2]) {
+  score++;
+  console.log("Well done !");
+  console.log("score = " + score);
+} else {
+  console.log("Try again !");
+  console.log("score final = " + score + " sur 3");
 }
