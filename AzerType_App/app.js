9e46fd24bd5ecefcaaf7160570b4f88915b2867e
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2024-02-18 22:08:39 +0100

    feat : clean exo

--- AzerType_App/app.js
+++ AzerType_App/app.js
@@ -2,11 +2,10 @@ const initialWord = ["paris", "noir", "renard"];
 
 let score = 0;
 
-let sentence = prompt("enter le mot : " + initialWord[0]);
-
 // ============================================
 // EXO CONDITION
 // ============================================
+// let sentence = prompt("enter le mot : " + initialWord[0]);
 // if (sentence === initialWord[0]) {
 //   score++;
 //   console.log("Well done !");
@@ -38,15 +37,16 @@ let sentence = prompt("enter le mot : " + initialWord[0]);
 //   console.log("score final = " + score + " sur " + initialWord.length);
 // }
 
+// ============================================
+// EXO CONDITION BOUCLE FOR
+// ============================================
 for (let i = 0; i < initialWord.length; i++) {
   let sentence = prompt("enter le mot : " + initialWord[i]);
   if (sentence === initialWord[i]) {
     score++;
     console.log("Well done !");
-    console.log("score = " + score);
   } else {
     console.log("Try again !");
-    console.log("score = " + score);
   }
   console.log("score = " + score + " sur " + [i + 1]);
 }
