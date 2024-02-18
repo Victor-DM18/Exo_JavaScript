af3912c9dc734480ffe68ab342c9907258883a2f
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2024-02-18 22:07:02 +0100

    feat : exo boucle for

--- AzerType_App/app.js
+++ AzerType_App/app.js
@@ -4,33 +4,49 @@ let score = 0;
 
 let sentence = prompt("enter le mot : " + initialWord[0]);
 
-if (sentence === initialWord[0]) {
-  score++;
-  console.log("Well done !");
-  console.log("score = " + score);
-} else {
-  console.log("Try again !");
-  console.log("score = " + score);
-}
+// ============================================
+// EXO CONDITION
+// ============================================
+// if (sentence === initialWord[0]) {
+//   score++;
+//   console.log("Well done !");
+//   console.log("score = " + score);
+// } else {
+//   console.log("Try again !");
+//   console.log("score = " + score);
+// }
 
-sentence = prompt("enter le mot : " + initialWord[1]);
+// sentence = prompt("enter le mot : " + initialWord[1]);
 
-if (sentence === initialWord[1]) {
-  score++;
-  console.log("Well done !");
-  console.log("score = " + score);
-} else {
-  console.log("Try again !");
-  console.log("score = " + score);
-}
+// if (sentence === initialWord[1]) {
+//   score++;
+//   console.log("Well done !");
+//   console.log("score = " + score);
+// } else {
+//   console.log("Try again !");
+//   console.log("score = " + score);
+// }
 
-sentence = prompt("enter le mot : " + initialWord[2]);
+// sentence = prompt("enter le mot : " + initialWord[2]);
 
-if (sentence === initialWord[2]) {
-  score++;
-  console.log("Well done !");
-  console.log("score = " + score);
-} else {
-  console.log("Try again !");
-  console.log("score final = " + score + " sur " + initialWord.length);
+// if (sentence === initialWord[2]) {
+//   score++;
+//   console.log("Well done !");
+//   console.log("score = " + score);
+// } else {
+//   console.log("Try again !");
+//   console.log("score final = " + score + " sur " + initialWord.length);
+// }
+
+for (let i = 0; i < initialWord.length; i++) {
+  let sentence = prompt("enter le mot : " + initialWord[i]);
+  if (sentence === initialWord[i]) {
+    score++;
+    console.log("Well done !");
+    console.log("score = " + score);
+  } else {
+    console.log("Try again !");
+    console.log("score = " + score);
+  }
+  console.log("score = " + score + " sur " + [i + 1]);
 }
