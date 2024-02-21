4f900d33ffd4561a23680058ac2047edd3d32dca
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2024-02-21 21:26:03 +0100

    feat : main.jsvariables

--- AzerType_App/scripts/main.js
+++ AzerType_App/scripts/main.js
@@ -1,1 +1,8 @@
-startGame();
+// startGame();
+
+const wordInput = document.getElementById("text-input");
+const submitButton = document.getElementById("submit-input");
+const wordList = document.querySelector("#word");
+const score = document.querySelector("#score");
+
+console.log(score);
