f9777c4e9a4c474f3d4167049ead561947b0906d
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2024-02-23 23:48:17 +0100

    feat : clean css

--- AzerType_App/scripts/main.js
+++ AzerType_App/scripts/main.js
@@ -1,8 +1,7 @@
-startGame();
+// startGame();
 
 const wordInput = document.getElementById("text-input");
 const submitButton = document.getElementById("submit-input");
 const wordList = document.querySelector("#word");
-const score = document.querySelector("#score");
 
 console.log(score);
