371b6b3ee6c2e4cd3851c25d91652dba3c7eb512
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2024-03-14 21:07:22 +0100

    feat : create removePopup function

--- AzerType_App/scripts/popup.js
+++ AzerType_App/scripts/popup.js
@@ -1,6 +1,13 @@
 const shareBtn = document.getElementById("share");
 const shareSection = document.getElementById("popup");
+const submitShare = document.getElementById("submit");
 
 shareBtn.addEventListener("click", (e) => {
   shareSection.classList.add("active");
 });
+
+const removePopup = () => {
+  submitShare.addEventListener("click", () => {
+    shareSection.classList.remove("active");
+  });
+};
