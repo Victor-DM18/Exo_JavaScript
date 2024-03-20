4674f6e759310f96fd433e76c0c05cae33966948
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2024-03-20 23:00:22 +0100

    feat : add all pieces in the list

--- Les_bonne_piece/Front_end/pieces.js
+++ Les_bonne_piece/Front_end/pieces.js
@@ -1,32 +1,33 @@
 const filtreSection = document.querySelector(".fiches");
-const imgElement = document.createElement("img");
-const nameElement = document.createElement("h2");
-const priceElement = document.createElement("p");
-const categoryElement = document.createElement("p");
-const descriptionElement = document.createElement("p");
-const stockElement = document.createElement("p");
 
 const res = await fetch("./pieces-autos.json");
 const pieces = await res.json();
 
-const article = pieces[0];
+for (let i = 0; i < pieces.length; i++) {
+  const imgElement = document.createElement("img");
+  const nameElement = document.createElement("h2");
+  const priceElement = document.createElement("p");
+  const categoryElement = document.createElement("p");
+  const descriptionElement = document.createElement("p");
+  const stockElement = document.createElement("p");
 
-imgElement.src = article.image;
-nameElement.innerText = article.nom;
-priceElement.innerText = `Prix : ${article.prix} € (${
-  article.prix < 35 ? "€" : "€€€"
-})`;
-categoryElement.innerText = article.categorie ?? "Aucune catégorie";
-descriptionElement.innerText = `${
-  article.description ?? "Aucune déscription pour le moment"
-}`;
-stockElement.innerText = `${
-  article.disponibilité ? "En stock" : "En rupture de stock"
-}`;
+  imgElement.src = pieces[i].image;
+  nameElement.innerText = pieces[i].nom;
+  priceElement.innerText = `Prix : ${pieces[i].prix} € (${
+    pieces[i].prix < 35 ? "€" : "€€€"
+  })`;
+  categoryElement.innerText = pieces[i].categorie ?? "Aucune catégorie";
+  descriptionElement.innerText = `${
+    pieces[i].description ?? "Aucune déscription pour le moment"
+  }`;
+  stockElement.innerText = `${
+    pieces[i].disponibilité ? "En stock" : "En rupture de stock"
+  }`;
 
-filtreSection.appendChild(imgElement);
-filtreSection.appendChild(nameElement);
-filtreSection.appendChild(priceElement);
-filtreSection.appendChild(categoryElement);
-filtreSection.appendChild(descriptionElement);
-filtreSection.appendChild(stockElement);
+  filtreSection.appendChild(imgElement);
+  filtreSection.appendChild(nameElement);
+  filtreSection.appendChild(priceElement);
+  filtreSection.appendChild(categoryElement);
+  filtreSection.appendChild(descriptionElement);
+  filtreSection.appendChild(stockElement);
+}
