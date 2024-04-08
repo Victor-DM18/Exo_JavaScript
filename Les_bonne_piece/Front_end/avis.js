120768883fa263ca5c3fb6a0e80d93b7b419af94
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2024-04-08 22:21:33 +0200

    feat : fix serverr

--- Les_bonne_piece/Front_end/avis.js
+++ Les_bonne_piece/Front_end/avis.js
@@ -3,7 +3,7 @@ export const displayOpinionList = () => {
   for (let i = 0; i < pieceElement.length; i++) {
     pieceElement[i].addEventListener("click", async (e) => {
       const id = e.target.dataset.id;
-      fetch(`http://localhost:8080/pieces/${id}/avis`);
+      fetch(`http://localhost:8081/pieces/${id}/avis`);
     });
   }
 };
--- Les_bonne_piece/Front_end/pieces.js
+++ Les_bonne_piece/Front_end/pieces.js
@@ -73,7 +73,7 @@ const displayArtcilesList = (pieces) => {
       pieces[i].disponibilité ? "En stock" : "En rupture de stock"
     }`;
     opinionBtn.innerText = "Voir les avis";
-    opinionBtn.dataset.id = piexeElement.id;
+    opinionBtn.dataset.id = pieces[i].id;
     opinionBtn.classList.add("opinion-button");
 
     listArticles.appendChild(piexeElement);
