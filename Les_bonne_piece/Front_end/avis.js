d3edbf572351810109f09567cb5c37759c997ab4
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2024-04-10 22:59:18 +0200

    feat : add await

--- Les_bonne_piece/Front_end/avis.js
+++ Les_bonne_piece/Front_end/avis.js
@@ -3,7 +3,7 @@ export const displayOpinionList = () => {
   for (let i = 0; i < pieceElement.length; i++) {
     pieceElement[i].addEventListener("click", async (e) => {
       const id = e.target.dataset.id;
-      fetch(`http://localhost:8081/pieces/${id}/avis`);
+      await fetch(`http://localhost:8081/pieces/${id}/avis`);
     });
   }
 };
