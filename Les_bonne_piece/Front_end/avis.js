f7ed343a5e8187e1aa065c3d827f00aac9a85e0a
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2024-04-11 22:22:11 +0200

    feat : update avis.js

--- Les_bonne_piece/Front_end/avis.js
+++ Les_bonne_piece/Front_end/avis.js
@@ -1,4 +1,5 @@
 export const displayOpinionList = () => {
+  const opinionList = document.querySelector(".opinion-list");
   const pieceElement = document.querySelectorAll(".fiches article button");
   for (let i = 0; i < pieceElement.length; i++) {
     pieceElement[i].addEventListener("click", async (e) => {
@@ -10,6 +11,8 @@ export const displayOpinionList = () => {
       const opinionElement = e.target.parentElement;
 
       const newOpinion = document.createElement("p");
+      newOpinion.classList.add("opinion-list");
+
       for (let i = 0; i < opinion.length; i++) {
         newOpinion.innerHTML += `<p>Nom : ${opinion[i].utilisateur} <br> Commentaire : ${opinion[i].commentaire}</p> `;
       }
