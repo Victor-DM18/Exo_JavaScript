158fdc9c809844da813cb495145cfb64f4131435
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2024-04-30 23:05:51 +0200

    feat : displayGraphOpinion function updted

--- Les_bonne_piece/Front_end/avis.js
+++ Les_bonne_piece/Front_end/avis.js
@@ -55,12 +55,39 @@ export const addOpinion = () => {
   });
 };
 
-export async function displayGraphOpinion() {}
-// Calcul du nombre total de commentaires par quantité d'étoiles attribuées
-const avis = await fetch("http://localhost:8081/avis").then((avis) =>
-  avis.json()
-);
-const nb_commentaires = [0, 0, 0, 0, 0];
-for (let commentaire of avis) {
-  nb_commentaires[commentaire.nbEtoiles - 1]++;
+export async function displayGraphOpinion() {
+  const labels = ["5", "4", "3", "2", "1"];
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Étoiles attribuées",
+        data: nb_commentaires.reverse(),
+        backgroundColor: "rgba(255, 230, 0, 1)", // couleur jaune
+      },
+    ],
+  };
+
+  const config = {
+    type: "bar",
+    data: data,
+    options: {
+      indexAxis: "y",
+    },
+  };
+
+  const graphiqueAvis = new Chart(
+    document.querySelector("#graphique-avis"),
+    config
+  );
+
+  // Calcul du nombre total de commentaires par quantité d'étoiles attribuées
+  const avis = await fetch("http://localhost:8081/avis").then((avis) =>
+    avis.json()
+  );
+  const nb_commentaires = [0, 0, 0, 0, 0];
+  for (let commentaire of avis) {
+    nb_commentaires[commentaire.nbEtoiles - 1]++;
+  }
 }
